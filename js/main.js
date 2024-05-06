846375f796d13f69e5f3e7e0380d9b8d44ec88dd
SaraBell99 <167412597+SaraBell99@users.noreply.github.com>
2024-05-06 16:52:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,3 +18,76 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+  // Create the data table.
+   var data = new google.visualization.DataTable();
+   data.addColumn('string', 'Topping');
+   data.addColumn('number', 'Slices');
+   data.addRows([
+   ['Maschi', 45],
+   ['Femmine', 67],
+  ]);
+
+ // Set chart options
+ var options = {'title':'Il genere',
+                'width':400,
+                'height':300,
+                'titleTextStyle': {fontSize: 20},
+                'legend': {textStyle: {fontSize: 13}}
+               };
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+      }
+
+
+var downloadLink = document.getElementById('downloadLink');
+
+// Ascolta l'evento 'click' sull'elemento di ancoraggio
+downloadLink.addEventListener('click', function(event) {
+    // Impedisce il comportamento predefinito del link (aprire l'URL specificato)
+    event.preventDefault();
+    
+    // URL del file da scaricare
+    var fileUrl = 'download/Metadati_schema_Zanichelli.xlsx'; // Sostituisci con l'URL effettivo del file
+
+    // Crea un oggetto XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    
+    // Imposta il metodo e l'URL della richiesta
+    xhr.open('GET', fileUrl, true);
+    
+    // Imposta il tipo di risposta come 'blob' (Binary Large Object)
+    xhr.responseType = 'blob';
+    
+    // Quando la richiesta è completata
+    xhr.onload = function() {
+        // Controlla se la richiesta ha avuto successo (status 200) 
+        if (this.status === 200) {
+            // Crea un oggetto URL dal blob restituito dalla richiesta
+            var blob = new Blob([this.response], { type: 'application/octet-stream' });
+            var url = window.URL.createObjectURL(blob);
+            
+            // Imposta l'attributo 'href' dell'elemento di ancoraggio per puntare all'URL del blob
+            downloadLink.href = url;
+            
+            // Forza il download del file facendo clic sul link
+            downloadLink.click();
+            
+            // Libera la risorsa URL creata
+            window.URL.revokeObjectURL(url);
+        }
+    };
+    
+    // Invia la richiesta
+    xhr.send();
+});
+
